d4984d00e55df881fcc45b41ab87eda718de36f7
Duncan Fairbanks <bonsairobo@gmail.com>
2016-04-19 03:54:45 -0400

    cmdline parsing, VGG model

---
+++ imgstyle.jl
@@ -0,0 +1,33 @@
+#!/usr/bin/julia
+
+function imgstyle(arg_map)
+
+end
+
+using DocOpt
+
+usage = """IMGStyle.
+
+Usage:
+    imgstyle.jl [options] <content_img> <style_img> 
+    imgstyle.jl -h | --help
+
+Options:
+    -h, --help                      Show this screen.
+    --output OUT_NAME               Provide a name for the output file. [default: out.png]
+    --num_iter N_ITER               [default: 1000]
+    --save_iter SAVE_ITER           [default: 100]
+    --print_iter PRINT_ITER         [default: 10]
+    --learning_rate LEARNING_RATE   [default: 0.1]
+    --style_weight STYLE_WEIGHT     [default: 0.5]
+    --content_weight CONTENT_WEIGHT [default: 0.5]
+    --content_layer CONTENT_LAYER   [default: relu4_2]
+    --style_layers STYLE_LAYERS     [default: relu1_1,relu2_1,relu3_1,relu4_1,relu5_1]
+    --init_style
+    --init_content
+    --init_img INIT_IMG
+"""
+
+arg_map = docopt(usage)
+
+imgstyle(arg_map)
---
+++ wget_model.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p model
+cd model
+wget https://github.com/dmlc/web-data/raw/master/mxnet/neural-style/model/vgg19.params
+cd ..
