1295cfb37e73cee77c040c5a5bf23831a5ec7f0f
Duncan Fairbanks <bonsairobo@gmail.com>
2016-04-22 02:08:46 -0400

    stylenet initialization complete

--- imgstyle.jl
+++ imgstyle.jl
@@ -1,74 +1,14 @@
 #!/usr/bin/julia
 
-using MXNet, Images, Colors, FixedPointNumbers, DocOpt
+using MXNet, Images, DocOpt
 
-# Imagenet mean pixel
-mean_rgb = (103.939, 116.779, 123.68)
-
-# VGG19 expects a specific image format
-function preprocess_vgg(img)
-    arr = 256 * data(separate(convert(Image{BGR{Float32}}, img)))
-    arr[:,:,1] -= mean_rgb[3]
-    arr[:,:,2] -= mean_rgb[2]
-    arr[:,:,3] -= mean_rgb[1]
-    return arr
-end
-
-# Undo preprocessing
-function postprocess_vgg(arr)
-    arr[:,:,1] += mean_rgb[3]
-    arr[:,:,2] += mean_rgb[2]
-    arr[:,:,3] += mean_rgb[1]
-    arr /= 256
-    return convert(Image{RGB{Float32}}, arr)
-end
+include("stylenet.jl")
 
 # E.g. "1,2,3" -> (1,2,3)
 str2inttup(str) = tuple(map(x -> parse(Int32, x), split(str, ','))...)
 
 # E.g. "relu1_1,relu2_2" -> [relu1_1, relu2_2]
 str2symbols(str) = map(mx.Variable, split(str, ','))
-
-type GramianExecutor
-    executor :: mx.Executor
-    data :: mx.NDArray
-    data_grad :: mx.NDArray
-end
-
-function make_gramian_executor(in_shape, context)
-    # Each entry of the Gramian matrix is the correllation between activations
-    # of two filters in a layer
-    style_relu1 = mx.Variable(:style_relu1)
-    style_relu2 = mx.Variable(:style_relu2)
-
-    # Flatten the activation for each filter to form a matrix where each column
-    # is one filter's activation
-    flat_shape = (reduce(*, in_shape[1:end-1]), in_shape[end])
-    flat_act1 = mx.Reshape(data=style_relu1, target_shape=flat_shape)
-    flat_act2 = mx.Reshape(data=style_relu2, target_shape=flat_shape)
-
-    # Dot all 2-combinations of activations to create Gramian. This is
-    # equivalent to matrix self-multiplication with transpose or a MLP where the
-    # input and weights are each others transpose.
-    flat_act2 = mx.SwapAxis(data=flat_act2, dim1=)
-    fc = mx.FullyConnected(data=flat_act1, weight=flat_act2, no_bias=true,
-        num_hidden=in_shape[end])
-
-    # Bind the same NDArray to both
-    data_nd = mx.zeros(in_shape, context)
-    data_grad_nd = mx.zeros(in_shape, context)
-    exec = fc.bind()
-
-    return GramianExecutor(exec, )
-end
-
-function style_representation()
-
-end
-
-function content_representation()
-
-end
 
 usage = """IMGStyle.
 
@@ -86,21 +26,19 @@ Options:
     --learning_rate LEARNING_RATE   [default: 0.1]
     --style_weight STYLE_WEIGHT     [default: 0.5]
     --content_weight CONTENT_WEIGHT [default: 0.5]
-    --content_layer CONTENT_LAYER   [default: relu4_2]
+    --content_layers CONTENT_LAYERS [default: relu4_2]
     --style_layers STYLE_LAYERS     [default: relu1_1,relu2_1,relu3_1,relu4_1,relu5_1]
     --init_style
     --init_content
     --init_img INIT_IMG
 """
-arg_map = docopt(usage)
+option_map = docopt(usage)
 
-gpu_context = mx.gpu()
-
-content_img = imread(arg_map["<content_image>"])
-style_img = imread(arg_map["<style_image>"])
+content_img = imread(option_map["<content_image>"])
+style_img = imread(option_map["<style_image>"])
 
 # Determine output resolution
-out_size_str = arg_map["--output_size"]
+out_size_str = option_map["--output_size"]
 out_size = if out_size_str == nothing
     size(content_img)
 else
@@ -112,22 +50,11 @@ end
 # (Gramian), where shape is defined only by the # of filters.
 content_img = Images.imresize(content_img, out_size)
 
-content_arr = preprocess_vgg19(content_img)
-style_arr = preprocess_vgg19(style_img)
-
-# Create GPU-backed NDArrays for input images
-content_nd = mx.copy(content_arr, gpu_context)
-style_nd = mx.copy(style_arr, gpu_context)
-
 # Get symbols for specified layers
-style_layers = str2symbols(arg_map["--style_layers"])
-content_layer = str2symbols(arg_map["--content_layer"])
+style_layers = str2symbols(option_map["--style_layers"])
+content_layers = str2symbols(option_map["--content_layers"])
 
-# Multiple executors form the entire network
-vgg_exec =
-    make_vgg19_executor(out_size, style_layers, content_layer, gpu_context)
-gram_execs = [make_gramian_executor(size(ndarr), gpu_context)
-    for ndarr in vgg_exec.style_activations]
+stylenet = StyleNet(
+    mx.gpu(), content_img, style_img, content_layers, style_layers)
 
-style_rep = style_representation()
-content_rep = content_representation()
+
--- stylenet.jl
+++ stylenet.jl
@@ -1,62 +1,116 @@
 using MXNet
 
-type Gramian
-	data :: mx.NDArray
-	data_grad :: mx.NDArray
-	style_repr_nd :: mx.NDArray
-end
-
-function get_style_repr(style_img_nd)
-
-end
-
-function make_gramian(in_shape)
-	# Each entry of the Gramian matrix is the correllation between activations
-    # of two filters in a layer
-    style_relu1 = mx.Variable(:style_relu1)
-    style_relu2 = mx.Variable(:style_relu2)
-
-    # Flatten the activation for each filter to form a matrix where each column
-    # is one filter's activation
-    flat_shape = (reduce(*, in_shape[1:end-1]), in_shape[end])
-    flat_act1 = mx.Reshape(data=style_relu1, target_shape=flat_shape)
-    flat_act2 = mx.Reshape(data=style_relu2, target_shape=flat_shape)
-
-    # Dot all 2-combinations of activations to create Gramian. This is
-    # equivalent to matrix self-multiplication with transpose or a MLP where the
-    # input and weights are each others transpose.
-    flat_act2 = mx.SwapAxis(data=flat_act2, dim1=)
-    fc = mx.FullyConnected(data=flat_act1, weight=flat_act2, no_bias=true,
-        num_hidden=in_shape[end])
-        
-    return Gramian()
-end
-
-type L2Norm
-	data :: mx.NDArray
-	data_grad :: mx.NDArray
-end
-
-type TotalVariation
-	data :: mx.NDArray
-	data_grad :: mx.NDArray
-end
+include("vggnet.jl")
 
 type StyleNet
-	exec :: mx.Executor
-	gram :: Gramian
-	dist :: L2Norm
-	total_var :: TotalVariation
+    ctx :: mx.Context
+
+    # All output (loss) nodes & arguments in the network
+    node :: mx.SymbolicNode
+
+    # Executor for back/forward propagation
+    exec
+
+    # Arguments and their gradients (provided to create Executor)
+    arg_map :: Dict{Symbol, mx.NDArray}
+    grad_map :: Dict{Symbol, mx.NDArray}
+
+    # Parallel arrays
+    style_layers :: Array{Symbol,1}
+    style_repr :: Array{mx.NDArray,1} # Gramian matrices
+    style_grad :: Array{mx.NDArray,1} # L2 gradients
+
+    content_layers :: Array{Symbol,1}
+    content_repr :: Array{mx.NDArray,1} # ReLU outputs
+    content_grad :: Array{mx.NDArray,1} # L2 gradients
+
+    # Total Variation gradient for image
+    tv_grad :: mx.NDArray
+
+    function StyleNet(ctx, content_img, style_img, content_layers, style_layers)
+        style_arr = preprocess_vgg(style_img)
+        content_arr = preprocess_vgg(content_img)
+        style_data_size = (size(style_arr)...,1)
+        content_data_size = (size(content_arr)...,1)
+
+        # Initialize symbolic graph with VGG CNN
+        make_vggnet()
+        node = mx.Group(content_layers..., style_layers...)
+        arg_shapes, out_shapes, = mx.infer_shape(node, data=style_data_size)
+
+        # Replace style layer outputs with Gramian outputs
+        node = mx.Group(content_layers...)
+        num_content = size(content_layers, 1)
+        for (i, layer) in enumerate(style_layers)
+            gram = make_gramian(layer, out_shapes[num_content + i])
+            node = mx.Group(node, gram)
+        end
+
+        # Allocate GPU memory for arguments and their gradients
+        arg_map = Dict()
+        grad_map = Dict()
+        arg_names = mx.list_arguments(node)
+        load_arguments!(
+            arg_names, arg_shapes, arg_map, grad_map, "model/vgg19.params")
+
+        # Create executor for calculating style representation
+        net = new(ctx, node, nothing, arg_map, grad_map, style_layers, [],
+            fill(mx.zeros(0), num_style), content_layers, [],
+            fill(mx.zeros(0), num_content))
+        reset_executor!(net)
+
+        # Get Gramian matrices for style image
+        net.arg_map[:data][:] = style_arr
+        mx.forward(net.exec)
+        num_style = size(style_layers, 1)
+        for i = 1:num_style
+            push!(net.style_repr, net.exec.outputs[num_content + i])
+        end
+
+        # Fit network outputs for the content image
+        net.arg_map[:data] =
+            mx.copy(reshape(content_arr, content_data_size), net.ctx)
+        net.grad_map[:data] = mx.zeros(content_data_size, net.ctx)
+        reset_executor!(net)
+
+        # Get ReLU output for content image
+        mx.forward(net.exec)
+        for i = 1:num_content
+            push!(net.content_repr, net.exec.outputs[i])
+        end
+
+        return net
+    end
 end
 
-function make_stylenet()
-	# Group loss layers for convenient access to all unbound arguments
-    # (data, weights, biases) on which the loss is dependent
-    style_group = mx.Group(style_layers...)
-    content_group = mx.Group(content_layer...)
-    loss_group = mx.Group(style_group, content_group)
-    
-	get_style_repr()
-	
-	return StyleNet()
+function load_arguments!(
+    arg_names :: Array{Symbol,1}
+    arg_shapes :: Array{Tuple,1}
+    arg_map :: Dict{Symbol, mx.NDArray},
+    grad_map :: Dict{Symbol, mx.NDArray},
+    model_path :: AbstractString)
+
+    # Model is pre-trained. Get map from symbol to NDArray of weights or biases.
+    pretrain = mx.load(model_path, mx.NDArray)
+
+    # Zero-initialize arguments and gradients
+    grad_map = Dict(zip(arg_names, [mx.zeros(shape) for shape in arg_shapes]))
+    arg_map = Dict(zip(arg_names, [mx.zeros(shape) for shape in arg_shapes]))
+
+    # Copy pre-trained weights and biases into new NDArrays
+    for name in arg_names[2:end] # skip :data
+        arg_map[name] = pretrain[symbol("arg:" * string(name))]
+    end
+    gc() # Clean up unused zero NDArrays
+end
+
+function reset_executor!(net :: StyleNet)
+    if net.exec != nothing
+        # Free GPU memory
+        net.exec.outputs = []
+        gc()
+    end
+
+    # Create new executor with different input/output shapes
+    net.exec = mx.bind(net.node, net.ctx, net.arg_map, args_grad=net.grad_map)
 end
